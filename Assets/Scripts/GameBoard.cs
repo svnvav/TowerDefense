5726f7e50a56e959a261fc939b000ebb359ab9d9
svnvav <svnvav@mail.ru>
2019-11-05 19:44:46 +0300

    Pathfinding

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 
 namespace Catlike.TowerDefense
@@ -12,6 +13,8 @@ namespace Catlike.TowerDefense
         private Vector2Int size;
         
         private GameTile[] tiles;
+        
+        private Queue<GameTile> searchFrontier = new Queue<GameTile>();
 
         public void Initialize(Vector2Int size)
         {
@@ -31,7 +34,50 @@ namespace Catlike.TowerDefense
                     tile.transform.localPosition = new Vector3(
                         x - offset.x, 0f, y - offset.y
                     );
+                    if (x > 0) {
+                        GameTile.MakeEastWestNeighbors(tile, tiles[i - 1]);
+                    }
+                    if (y > 0) {
+                        GameTile.MakeNorthSouthNeighbors(tile, tiles[i - size.x]);
+                    }
+                    tile.IsAlternative = (x & 1) == 0;
+                    if ((y & 1) == 0) {
+                        tile.IsAlternative = !tile.IsAlternative;
+                    }
                 }
+            }
+            
+            FindPaths();
+        }
+        
+        private void FindPaths () {
+            foreach (GameTile tile in tiles) {
+                tile.ClearPath();
+            }
+            tiles[tiles.Length / 2].BecomeDestination();
+            searchFrontier.Enqueue(tiles[tiles.Length / 2]);
+            
+            while (searchFrontier.Count > 0) {
+                GameTile tile = searchFrontier.Dequeue();
+                if (tile != null)
+                {
+                    if (tile.IsAlternative) {
+                        searchFrontier.Enqueue(tile.GrowPathNorth());
+                        searchFrontier.Enqueue(tile.GrowPathSouth());
+                        searchFrontier.Enqueue(tile.GrowPathEast());
+                        searchFrontier.Enqueue(tile.GrowPathWest());
+                    }
+                    else {
+                        searchFrontier.Enqueue(tile.GrowPathWest());
+                        searchFrontier.Enqueue(tile.GrowPathEast());
+                        searchFrontier.Enqueue(tile.GrowPathSouth());
+                        searchFrontier.Enqueue(tile.GrowPathNorth());
+                    }
+                }
+            }
+            
+            foreach (GameTile tile in tiles) {
+                tile.ShowPath();
             }
         }
     }
--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -4,6 +4,76 @@ namespace Catlike.TowerDefense
 {
     public class GameTile : MonoBehaviour
     {
+        private static Quaternion
+            northRotation = Quaternion.Euler(90f, 0f, 0f),
+            eastRotation = Quaternion.Euler(90f, 90f, 0f),
+            southRotation = Quaternion.Euler(90f, 180f, 0f),
+            westRotation = Quaternion.Euler(90f, 270f, 0f);
+        
         [SerializeField] private Transform arrow = default;
+        
+        private GameTile north, east, south, west, nextOnPath;
+
+        private int distance;
+        
+        public bool HasPath => distance != int.MaxValue;
+        
+        public bool IsAlternative { get; set; }
+        
+        public GameTile GrowPathNorth () => GrowPathTo(north);
+
+        public GameTile GrowPathEast () => GrowPathTo(east);
+
+        public GameTile GrowPathSouth () => GrowPathTo(south);
+
+        public GameTile GrowPathWest () => GrowPathTo(west);
+        
+        private GameTile GrowPathTo (GameTile neighbor) {
+            if (!HasPath || neighbor == null || neighbor.HasPath) {
+                return null;
+            }
+            neighbor.distance = distance + 1;
+            neighbor.nextOnPath = this;
+            return neighbor;
+        }
+        
+        public void ClearPath () {
+            distance = int.MaxValue;
+            nextOnPath = null;
+        }
+        
+        public void ShowPath () {
+            if (distance == 0) {
+                arrow.gameObject.SetActive(false);
+                return;
+            }
+            arrow.gameObject.SetActive(true);
+            arrow.localRotation =
+                nextOnPath == north ? northRotation :
+                nextOnPath == east ? eastRotation :
+                nextOnPath == south ? southRotation :
+                westRotation;
+        }
+        
+        public void BecomeDestination () {
+            distance = 0;
+            nextOnPath = null;
+        }
+        
+        public static void MakeEastWestNeighbors (GameTile east, GameTile west) {
+            Debug.Assert(
+                west.east == null && east.west == null, "Redefined neighbors!"
+            );
+            west.east = east;
+            east.west = west;
+        }
+        
+        public static void MakeNorthSouthNeighbors (GameTile north, GameTile south) {
+            Debug.Assert(
+                south.north == null && north.south == null, "Redefined neighbors!"
+            );
+            south.north = north;
+            north.south = south;
+        }
     }
 }
